bd951b524a9417a36b99eaaca11cb826ca3602ca
sakil-sagor <sakilsagor76543@gmail.com>
2022-02-09 16:30:04 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {/* <AuthProvider>
+        <Router>
+          <Navbar></Navbar>
+          <ScrollToTop />
+          <Switch>
+            <Route exact path="/">
+              <Home></Home>
+            </Route>
+            <Route exact path="/home">
+              <Home></Home>
+            </Route>
+            <Route exact path="/shop">
+              <Shop></Shop>
+            </Route>
+            <PrivateRoute exact path="/addToCart">
+              <AddToCart></AddToCart>
+            </PrivateRoute>
+            <Route exact path="/shop/:productId">
+              <ProductDetails></ProductDetails>
+            </Route>
+            <PrivateRoute exact path="/directOrder/:productId">
+              <DirectOrder></DirectOrder>
+            </PrivateRoute>
+            <PrivateRoute exact path="/addToCartOrder/:addToCartId">
+              <AddToCartOrder></AddToCartOrder>
+            </PrivateRoute>
+            <PrivateRoute exact path="/orderNow">
+              <OrderNow></OrderNow>
+            </PrivateRoute>
+            <Route exact path="/aboutUs">
+              <AboutUs></AboutUs>
+            </Route>
+            <Route exact path="/contactUs">
+              <ContactUs></ContactUs>
+            </Route>
+            <Route exact path="/login">
+              <Login></Login>
+            </Route>
+            <Route exact path="/register">
+              <Register></Register>
+            </Route>
+            <Route exact path="/forgatePass">
+              <ForgatePass></ForgatePass>
+            </Route>
+            <Route exact path="/afterResetPass">
+              <AfterResetPass></AfterResetPass>
+            </Route>
+            <PrivateRoute path="/dashboard">
+              <Dashboard></Dashboard>
+            </PrivateRoute>
+            <PrivateRoute exact path="/addProducts">
+              <AddProduct></AddProduct>
+            </PrivateRoute>
+            <PrivateRoute exact path="/myOrders">
+              <MyOrders></MyOrders>
+            </PrivateRoute>
+            <PrivateRoute exact path="/manageOrders">
+              <ManageOrders></ManageOrders>
+            </PrivateRoute>
+            <PrivateRoute exact path="/payment">
+              <Payment></Payment>
+            </PrivateRoute>
+            <PrivateRoute exact path="/orderNow/:paymentId">
+              <Payment></Payment>
+            </PrivateRoute>
+            <PrivateRoute exact path="/myReview">
+              <MyReview></MyReview>
+            </PrivateRoute>
+            <PrivateRoute exact path="/viewProfile">
+              <ViewProfile></ViewProfile>
+            </PrivateRoute>
+            <PrivateRoute exact path="/manageProducts">
+              <ManageProducts></ManageProducts>
+            </PrivateRoute>
+            <Route path="*">
+              <NotFound></NotFound>
+            </Route>
+          </Switch>
+          <Footer></Footer>
+        </Router>
+      </AuthProvider> */}
+
     </div>
   );
 }
